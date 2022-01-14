f5c604282f4e0f208d6fd7cccde2bca09a188a74
xw <1804859224@qq.com>
2022-01-14 10:30:49 +0800

    forth commit

--- src/com/company/lingdian/Sort.java
+++ src/com/company/lingdian/Sort.java
@@ -59,5 +59,6 @@ public class Sort {
         quick_sort( num, 0, num.length-1);
         System.out.println(Arrays.toString(num));
         System.out.println("fix");
+        System.out.println("la");
     }
 }
